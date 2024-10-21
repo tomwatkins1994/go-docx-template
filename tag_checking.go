0cd8506195ffb74a07be454337c61d38e38ec0fb
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-21 12:59:59 +0100

    Use sync.Once to remove repeated compiling

--- tag_checking.go
+++ tag_checking.go
@@ -1,19 +1,35 @@
 package docxtpl
 
-import "regexp"
+import (
+	"regexp"
+	"sync"
+)
+
+var tagRegex *regexp.Regexp
+var tagOnce sync.Once
 
 func textContainsTags(text string) (bool, error) {
-	r, err := regexp.Compile("{{.*?}}")
+	var err error
+	tagOnce.Do(func() {
+		tagRegex, err = regexp.Compile("{{.*?}}")
+	})
 	if err != nil {
 		return false, err
 	}
-	return r.MatchString(text), err
+
+	return tagRegex.MatchString(text), err
 }
 
+var incompleteTagRegex *regexp.Regexp
+var incompleteTagOnce sync.Once
+
 func textContainsIncompleteTags(text string) (bool, error) {
-	r, err := regexp.Compile("{{[^}]*?(}$|$)|{$|^}")
+	var err error
+	incompleteTagOnce.Do(func() {
+		incompleteTagRegex, err = regexp.Compile("{{[^}]*?(}$|$)|{$|^}")
+	})
 	if err != nil {
 		return false, err
 	}
-	return r.MatchString(text), err
+	return incompleteTagRegex.MatchString(text), err
 }
