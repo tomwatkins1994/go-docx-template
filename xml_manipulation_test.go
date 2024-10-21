d0c8d0061521de3357639fac2ced0b470866c6d9
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-21 14:00:33 +0100

    Add additonal table tag tests

--- xml_manipulation_test.go
+++ xml_manipulation_test.go
@@ -26,4 +26,26 @@ func TestReplaceTableRangeRows(t *testing.T) {
 			t.Fatalf("Text left is: %v", newXmlString)
 		}
 	})
+
+	t.Run("Range tag in other text", func(t *testing.T) {
+		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl>"
+		newXmlString, err := replaceTableRangeRows(xmlString)
+		if err != nil {
+			t.Fatalf("Error in basic range tag: %v", err)
+		}
+		if newXmlString != "<w:tbl>{{range . }}</w:tbl>" {
+			t.Fatalf("Text left is: %v", newXmlString)
+		}
+	})
+
+	t.Run("End tag in other text", func(t *testing.T) {
+		xmlString := "<w:tbl><w:tr>{{end}}</w:tr></w:tbl>"
+		newXmlString, err := replaceTableRangeRows(xmlString)
+		if err != nil {
+			t.Fatalf("Error in basic end tag: %v", err)
+		}
+		if newXmlString != "<w:tbl>{{end}}</w:tbl>" {
+			t.Fatalf("Text left is: %v", newXmlString)
+		}
+	})
 }
