83c5f2584c3d83fafe6009a5a0bc19b99c35666f
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-21 13:15:54 +0100

    Basic end table row test

--- xml_manipulation_test.go
+++ xml_manipulation_test.go
@@ -15,4 +15,15 @@ func TestReplaceTableRangeRows(t *testing.T) {
 			t.Fatalf("Text left is: %v", newXmlString)
 		}
 	})
+
+	t.Run("Basic end tag", func(t *testing.T) {
+		xmlString := "<w:tr>{{end}}</w:tr>"
+		newXmlString, err := replaceTableRangeRows(xmlString)
+		if err != nil {
+			t.Fatalf("Error in basic end tag: %v", err)
+		}
+		if newXmlString != "{{end}}" {
+			t.Fatalf("Text left is: %v", newXmlString)
+		}
+	})
 }
