e2ebc3aa65b71598498538bb06d32b61da3cc093
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-21 14:04:51 +0100

    Full table test

--- xml_manipulation_test.go
+++ xml_manipulation_test.go
@@ -31,7 +31,7 @@ func TestReplaceTableRangeRows(t *testing.T) {
 		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl>"
 		newXmlString, err := replaceTableRangeRows(xmlString)
 		if err != nil {
-			t.Fatalf("Error in basic range tag: %v", err)
+			t.Fatalf("Error in range tag in otther text: %v", err)
 		}
 		if newXmlString != "<w:tbl>{{range . }}</w:tbl>" {
 			t.Fatalf("Text left is: %v", newXmlString)
@@ -42,7 +42,7 @@ func TestReplaceTableRangeRows(t *testing.T) {
 		xmlString := "<w:tbl><w:tr>{{end}}</w:tr></w:tbl>"
 		newXmlString, err := replaceTableRangeRows(xmlString)
 		if err != nil {
-			t.Fatalf("Error in basic end tag: %v", err)
+			t.Fatalf("Error in end tag in other text: %v", err)
 		}
 		if newXmlString != "<w:tbl>{{end}}</w:tbl>" {
 			t.Fatalf("Text left is: %v", newXmlString)
@@ -53,7 +53,7 @@ func TestReplaceTableRangeRows(t *testing.T) {
 		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl><w:tbl><w:tr>{{range . }}</w:tr></w:tbl>"
 		newXmlString, err := replaceTableRangeRows(xmlString)
 		if err != nil {
-			t.Fatalf("Error in basic range tag: %v", err)
+			t.Fatalf("Error in multiple range tag: %v", err)
 		}
 		if newXmlString != "<w:tbl>{{range . }}</w:tbl><w:tbl>{{range . }}</w:tbl>" {
 			t.Fatalf("Text left is: %v", newXmlString)
@@ -64,9 +64,20 @@ func TestReplaceTableRangeRows(t *testing.T) {
 		xmlString := "<w:tbl><w:tr>{{end}}</w:tr></w:tbl><w:tbl><w:tr>{{end}}</w:tr></w:tbl>"
 		newXmlString, err := replaceTableRangeRows(xmlString)
 		if err != nil {
-			t.Fatalf("Error in basic range tag: %v", err)
+			t.Fatalf("Error in multiple end tags: %v", err)
 		}
 		if newXmlString != "<w:tbl>{{end}}</w:tbl><w:tbl>{{end}}</w:tbl>" {
+			t.Fatalf("Text left is: %v", newXmlString)
+		}
+	})
+
+	t.Run("Full table", func(t *testing.T) {
+		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr><w:tr></w:tr><w:tr>{{end}}</w:tr></w:tbl>"
+		newXmlString, err := replaceTableRangeRows(xmlString)
+		if err != nil {
+			t.Fatalf("Error in full table: %v", err)
+		}
+		if newXmlString != "<w:tbl>{{range . }}<w:tr></w:tr>{{end}}</w:tbl>" {
 			t.Fatalf("Text left is: %v", newXmlString)
 		}
 	})
