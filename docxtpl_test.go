59d298af1e03c3132dda7faa5622e0dd60c3c917
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-22 15:58:45 +0100

    Resizing in test

--- docxtpl_test.go
+++ docxtpl_test.go
@@ -129,6 +129,10 @@ func TestParseAndRender(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v - Inline image error: %v", t.Name(), err)
 		}
+		err = profileImage.Resize(200, 200)
+		if err != nil {
+			t.Fatalf("%v - Resizing inline image error: %v", t.Name(), err)
+		}
 
 		data := struct {
 			ProjectNumber string
