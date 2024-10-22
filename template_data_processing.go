0f7c9eecde4907ff3b46989f3268b1cf14ee247d
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-22 14:55:31 +0100

    Make addImage public

--- template_data_processing.go
+++ template_data_processing.go
@@ -33,7 +33,7 @@ func handleTagValues(d *DocxTmpl, data *map[string]interface{}) error {
 				return err
 			} else {
 				if isImage {
-					image, err := d.addImage(stringVal)
+					image, err := d.AddImage(stringVal)
 					if err != nil {
 						return err
 					}
