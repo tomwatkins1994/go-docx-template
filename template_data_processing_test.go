5b237c9615634a522b9142f3b685c722dd826017
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-10-22 14:27:49 +0100

    Test nested data

--- template_data_processing_test.go
+++ template_data_processing_test.go
@@ -1,6 +1,7 @@
 package docxtpl
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -45,4 +46,50 @@ func TestConvertStructToMap(t *testing.T) {
 		}
 	})
 
+	t.Run("Struct with nested data", func(t *testing.T) {
+		data := struct {
+			ProjectNumber string
+			Client        string
+			Status        string
+			People        []struct {
+				Name   string
+				Gender string
+				Age    uint8
+			}
+		}{
+			ProjectNumber: "B-00001",
+			Client:        "TW Software",
+			Status:        "New",
+			People: []struct {
+				Name   string
+				Gender string
+				Age    uint8
+			}{
+				{
+					Name:   "Tom Watkins",
+					Gender: "Male",
+					Age:    30,
+				},
+				{
+					Name:   "Evie Argyle",
+					Gender: "Female",
+					Age:    29,
+				},
+			},
+		}
+		mapData, err := convertStructToMap(data)
+		if err != nil {
+			t.Fatalf("Error converting struct with nested data: %v", err)
+		}
+		for key, value := range mapData {
+			val := reflect.ValueOf(value)
+			if val.Kind() == reflect.Slice {
+				for i := 0; i < val.Len(); i++ {
+					if val.Index(i).Kind() == reflect.Struct {
+						t.Fatalf("Found struct in data: %v %v", key, value)
+					}
+				}
+			}
+		}
+	})
 }
