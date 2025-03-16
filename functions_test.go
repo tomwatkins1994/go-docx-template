7eecfcbf1b3eaa0b93adb9b3907c8ca31ec69100
Tom Watkins <twwatkins@btinternet.com>
2025-03-16 19:39:17 +0000

    Extra tests on funcMap

--- functions_test.go
+++ functions_test.go
@@ -8,11 +8,16 @@ func TestRegisterFunctions(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v - Parsing error: %v", t.Name(), err)
 		}
-		err = doc.RegisterFunction("hello", func(text string) string {
+		name := "hello"
+		err = doc.RegisterFunction(name, func(text string) string {
 			return "hello"
 		})
 		if err != nil {
 			t.Fatalf("%v - Error registering function: %v", t.Name(), err)
+		}
+		funcMap := doc.GetRegisteredFunctions()
+		if (*funcMap)[name] == nil {
+			t.Fatalf("%v - function not found in map", t.Name())
 		}
 	})
 
@@ -21,11 +26,16 @@ func TestRegisterFunctions(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v - Parsing error: %v", t.Name(), err)
 		}
-		err = doc.RegisterFunction("hello", func(text string) (string, error) {
+		name := "hello"
+		err = doc.RegisterFunction(name, func(text string) (string, error) {
 			return "hello", nil
 		})
 		if err != nil {
 			t.Fatalf("%v - Error registering function: %v", t.Name(), err)
+		}
+		funcMap := doc.GetRegisteredFunctions()
+		if (*funcMap)[name] == nil {
+			t.Fatalf("%v - function not found in map", t.Name())
 		}
 	})
 
@@ -34,11 +44,16 @@ func TestRegisterFunctions(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v - Parsing error: %v", t.Name(), err)
 		}
-		err = doc.RegisterFunction("hello", func(text string) {
+		name := "hello"
+		err = doc.RegisterFunction(name, func(text string) {
 			// Do nothing here
 		})
 		if err == nil {
 			t.Fatalf("%v - Did not return expected error: %v", t.Name(), err)
+		}
+		funcMap := doc.GetRegisteredFunctions()
+		if (*funcMap)[name] != nil {
+			t.Fatalf("%v - function found in map when it shouldn't be: %v", t.Name(), (*funcMap)[name])
 		}
 	})
 }
