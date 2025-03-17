fcd53691e1942763ea7f033ba95ae101f8ac74d5
Tom Watkins <twwatkins@btinternet.com>
2025-03-17 00:19:49 +0000

    Use testing table in xml manipulation tests

--- xml_manipulation_test.go
+++ xml_manipulation_test.go
@@ -5,80 +5,57 @@ import (
 )
 
 func TestReplaceTableRangeRows(t *testing.T) {
-	t.Run("Basic range tag", func(t *testing.T) {
-		xmlString := "<w:tr>{{range . }}</w:tr>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in basic range tag: %v", err)
-		}
-		if newXmlString != "{{range . }}" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
+	tests := []struct {
+		name              string
+		inputXml          string
+		expectedOutputXml string
+	}{
+		{
+			name:              "Basic range tag",
+			inputXml:          "<w:tr>{{range . }}</w:tr>",
+			expectedOutputXml: "{{range . }}",
+		},
+		{
+			name:              "Basic end tag",
+			inputXml:          "<w:tr>{{end}}</w:tr>",
+			expectedOutputXml: "{{end}}",
+		},
+		{
+			name:              "Range tag in other text",
+			inputXml:          "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl>",
+			expectedOutputXml: "<w:tbl>{{range . }}</w:tbl>",
+		},
+		{
+			name:              "End tag in other text",
+			inputXml:          "<w:tbl><w:tr>{{end}}</w:tr></w:tbl>",
+			expectedOutputXml: "<w:tbl>{{end}}</w:tbl>",
+		},
+		{
+			name:              "Multiple range tags",
+			inputXml:          "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl><w:tbl><w:tr>{{range . }}</w:tr></w:tbl>",
+			expectedOutputXml: "<w:tbl>{{range . }}</w:tbl><w:tbl>{{range . }}</w:tbl>",
+		},
+		{
+			name:              "Multiple end tags",
+			inputXml:          "<w:tbl><w:tr>{{end}}</w:tr></w:tbl><w:tbl><w:tr>{{end}}</w:tr></w:tbl>",
+			expectedOutputXml: "<w:tbl>{{end}}</w:tbl><w:tbl>{{end}}</w:tbl>",
+		},
+		{
+			name:              "Full table",
+			inputXml:          "<w:tbl><w:tr>{{range . }}</w:tr><w:tr></w:tr><w:tr>{{end}}</w:tr></w:tbl>",
+			expectedOutputXml: "<w:tbl>{{range . }}<w:tr></w:tr>{{end}}</w:tbl>",
+		},
+	}
 
-	t.Run("Basic end tag", func(t *testing.T) {
-		xmlString := "<w:tr>{{end}}</w:tr>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in basic end tag: %v", err)
-		}
-		if newXmlString != "{{end}}" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
-
-	t.Run("Range tag in other text", func(t *testing.T) {
-		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in range tag in otther text: %v", err)
-		}
-		if newXmlString != "<w:tbl>{{range . }}</w:tbl>" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
-
-	t.Run("End tag in other text", func(t *testing.T) {
-		xmlString := "<w:tbl><w:tr>{{end}}</w:tr></w:tbl>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in end tag in other text: %v", err)
-		}
-		if newXmlString != "<w:tbl>{{end}}</w:tbl>" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
-
-	t.Run("Multiple range tags", func(t *testing.T) {
-		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr></w:tbl><w:tbl><w:tr>{{range . }}</w:tr></w:tbl>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in multiple range tag: %v", err)
-		}
-		if newXmlString != "<w:tbl>{{range . }}</w:tbl><w:tbl>{{range . }}</w:tbl>" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
-
-	t.Run("Multiple end tags", func(t *testing.T) {
-		xmlString := "<w:tbl><w:tr>{{end}}</w:tr></w:tbl><w:tbl><w:tr>{{end}}</w:tr></w:tbl>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in multiple end tags: %v", err)
-		}
-		if newXmlString != "<w:tbl>{{end}}</w:tbl><w:tbl>{{end}}</w:tbl>" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
-
-	t.Run("Full table", func(t *testing.T) {
-		xmlString := "<w:tbl><w:tr>{{range . }}</w:tr><w:tr></w:tr><w:tr>{{end}}</w:tr></w:tbl>"
-		newXmlString, err := replaceTableRangeRows(xmlString)
-		if err != nil {
-			t.Fatalf("Error in full table: %v", err)
-		}
-		if newXmlString != "<w:tbl>{{range . }}<w:tr></w:tr>{{end}}</w:tbl>" {
-			t.Fatalf("Text left is: %v", newXmlString)
-		}
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			outputXml, err := replaceTableRangeRows(tt.inputXml)
+			if err != nil {
+				t.Fatalf("Unexptected error: %v", err)
+			}
+			if outputXml != tt.expectedOutputXml {
+				t.Fatalf("output XML string `%v` does not match expected `%v`", outputXml, tt.expectedOutputXml)
+			}
+		})
+	}
 }
