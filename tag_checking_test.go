50bae43d27fae4a12f5fb330b5f83d25830885bf
Tom Watkins <twwatkins@btinternet.com>
2025-03-17 22:55:14 +0000

    Remove commented out code

--- tag_checking_test.go
+++ tag_checking_test.go
@@ -44,46 +44,6 @@ func TestTextContainsTags(t *testing.T) {
 }
 
 func TestTextContainsIncompleteTags(t *testing.T) {
-	// 	t.Run("Single partial tag", func(t *testing.T) {
-	// 		containsIncompleteTags, err := textContainsIncompleteTags("This text contains a partial tag: {{.Tag")
-	// 		if err != nil {
-	// 			t.Fatalf("Error checking single incomplete tag: %v", err)
-	// 		}
-	// 		if containsIncompleteTags == false {
-	// 			t.Fatalf("Should be true")
-	// 		}
-	// 	})
-
-	// 	t.Run("Multiple tags", func(t *testing.T) {
-	// 		containsIncompleteTags, err := textContainsIncompleteTags("This text contains multiple incomplete tags: {{.Tag1 and {{.Tag2")
-	// 		if err != nil {
-	// 			t.Fatalf("Error checking multiple tags: %v", err)
-	// 		}
-	// 		if containsIncompleteTags == false {
-	// 			t.Fatalf("Should be true")
-	// 		}
-	// 	})
-
-	// 	t.Run("No tag", func(t *testing.T) {
-	// 		containsIncompleteTags, err := textContainsIncompleteTags("This text contains no incomplete tags")
-	// 		if err != nil {
-	// 			t.Fatalf("Error checking single tag: %v", err)
-	// 		}
-	// 		if containsIncompleteTags == true {
-	// 			t.Fatalf("Should be false")
-	// 		}
-	// 	})
-
-	// 	t.Run("Complete tag", func(t *testing.T) {
-	// 		containsIncompleteTags, err := textContainsIncompleteTags("This text contains a tag {{.Tag}}")
-	// 		if err != nil {
-	// 			t.Fatalf("Error checking single tag: %v", err)
-	// 		}
-	// 		if containsIncompleteTags == true {
-	// 			t.Fatalf("Should be false")
-	// 		}
-	// 	})
-
 	tests := []struct {
 		name           string
 		text           string
