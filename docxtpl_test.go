d7bc1542139c3ae0b2c6965081505fa0d499bc88
Tom Watkins <twwatkins@btinternet.com>
2025-03-25 23:56:06 +0000

    Use test table

--- docxtpl_test.go
+++ docxtpl_test.go
@@ -9,213 +9,232 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type test struct {
+	name           string
+	filename       string
+	outputFilename string
+	data           any
+	fns            map[string]any
+}
+
+var myMap = make(map[string]int)
+
+func getTests() ([]test, error) {
+	testImage, err := CreateInlineImage("test_templates/test_image.png")
+	if err != nil {
+		return nil, err
+	}
+	profileImage, err := CreateInlineImage("test_templates/test_image.jpg")
+	if err != nil {
+		return nil, err
+	}
+
+	var tests = []test{
+		{
+			name:     "Basic document",
+			filename: "test_basic.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+			},
+		},
+		{
+			name:     "Basic document with images",
+			filename: "test_basic_with_images.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+				ImagePng      string
+				ImageJpg      string
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+				ImagePng:      "test_templates/test_image.png",
+				ImageJpg:      "test_templates/test_image.png",
+			},
+		},
+		{
+			name:     "Basic document with tables",
+			filename: "test_with_tables.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+				CreatedBy     string
+				SignedOffBy   string
+				People        []struct {
+					Name   string
+					Gender string
+					Age    uint8
+				}
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+				CreatedBy:     "Tom Watkins",
+				SignedOffBy:   "Tom Watkins",
+				People: []struct {
+					Name   string
+					Gender string
+					Age    uint8
+				}{
+					{
+						Name:   "Tom Watkins",
+						Gender: "Male",
+						Age:    30,
+					},
+					{
+						Name:   "Evie Argyle",
+						Gender: "Female",
+						Age:    29,
+					},
+				},
+			},
+		},
+		{
+			name:     "Basic document with tables and images",
+			filename: "test_with_tables_and_images.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+				Image         string
+				People        []struct {
+					Name           string
+					Gender         string
+					Age            uint8
+					ProfilePicture string
+				}
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+				Image:         "test_templates/test_image.png",
+				People: []struct {
+					Name           string
+					Gender         string
+					Age            uint8
+					ProfilePicture string
+				}{
+					{
+						Name:           "Tom Watkins",
+						Gender:         "Male",
+						Age:            30,
+						ProfilePicture: "test_templates/test_image.jpg",
+					},
+					{
+						Name:           "Evie Argyle",
+						Gender:         "Female",
+						Age:            29,
+						ProfilePicture: "test_templates/test_image.jpeg",
+					},
+				},
+			},
+		},
+		{
+			name:           "Basic document with image structs",
+			filename:       "test_with_tables_and_images.docx",
+			outputFilename: "test_with_image_structs.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+				Image         *InlineImage
+				People        []struct {
+					Name           string
+					Gender         string
+					Age            uint8
+					ProfilePicture *InlineImage
+				}
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+				Image:         testImage,
+				People: []struct {
+					Name           string
+					Gender         string
+					Age            uint8
+					ProfilePicture *InlineImage
+				}{
+					{
+						Name:           "Tom Watkins",
+						Gender:         "Male",
+						Age:            30,
+						ProfilePicture: profileImage,
+					},
+					{
+						Name:           "Evie Argyle",
+						Gender:         "Female",
+						Age:            29,
+						ProfilePicture: profileImage,
+					},
+				},
+			},
+		},
+		{
+			name:     "Basic document with custom functions",
+			filename: "test_with_custom_functions.docx",
+			data: struct {
+				ProjectNumber string
+				Client        string
+				Status        string
+			}{
+				ProjectNumber: "B-00001",
+				Client:        "TW Software",
+				Status:        "New",
+			},
+			fns: map[string]any{
+				"hello": func(text string) string {
+					return fmt.Sprintf("Hello %v", text)
+				},
+			},
+		},
+	}
+
+	return tests, nil
+}
+
 func TestParseAndRender(t *testing.T) {
-	t.Run("Basic document", func(t *testing.T) {
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-		}
-		parseAndRender(t, "test_basic.docx", data)
-	})
+	tests, err := getTests()
+	assert.Nil(t, err)
 
-	t.Run("Basic document with images", func(t *testing.T) {
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-			ImagePng      string
-			ImageJpg      string
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-			ImagePng:      "test_templates/test_image.png",
-			ImageJpg:      "test_templates/test_image.png",
-		}
-		parseAndRender(t, "test_basic_with_images.docx", data)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
 
-	t.Run("Document with tables", func(t *testing.T) {
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-			CreatedBy     string
-			SignedOffBy   string
-			People        []struct {
-				Name   string
-				Gender string
-				Age    uint8
+			doc, err := ParseFromFilename("test_templates/" + tt.filename)
+			assert.Nil(err, "Parsing error")
+
+			for fnName, fn := range tt.fns {
+				err = doc.RegisterFunction(fnName, fn)
+				assert.Nil(err)
 			}
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-			CreatedBy:     "Tom Watkins",
-			SignedOffBy:   "Tom Watkins",
-			People: []struct {
-				Name   string
-				Gender string
-				Age    uint8
-			}{
-				{
-					Name:   "Tom Watkins",
-					Gender: "Male",
-					Age:    30,
-				},
-				{
-					Name:   "Evie Argyle",
-					Gender: "Female",
-					Age:    29,
-				},
-			},
-		}
-		parseAndRender(t, "test_with_tables.docx", data)
-	})
 
-	t.Run("Document with tables and images", func(t *testing.T) {
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-			Image         string
-			People        []struct {
-				Name           string
-				Gender         string
-				Age            uint8
-				ProfilePicture string
+			err = doc.Render(tt.data)
+			assert.Nil(err, "Rendering error")
+
+			outputFilename := tt.filename
+			if len(tt.outputFilename) > 0 {
+				outputFilename = tt.outputFilename
 			}
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-			Image:         "test_templates/test_image.png",
-			People: []struct {
-				Name           string
-				Gender         string
-				Age            uint8
-				ProfilePicture string
-			}{
-				{
-					Name:           "Tom Watkins",
-					Gender:         "Male",
-					Age:            30,
-					ProfilePicture: "test_templates/test_image.jpg",
-				},
-				{
-					Name:           "Evie Argyle",
-					Gender:         "Female",
-					Age:            29,
-					ProfilePicture: "test_templates/test_image.jpeg",
-				},
-			},
-		}
-		parseAndRender(t, "test_with_tables_and_images.docx", data)
-	})
+			f, err := os.Create("test_templates/generated_" + outputFilename)
+			assert.Nil(err, "Error creating document")
 
-	t.Run("Document with image structs", func(t *testing.T) {
-		assert := assert.New(t)
+			err = doc.Save(f)
+			assert.Nil(err, "Error sacing document")
 
-		doc, err := ParseFromFilename("test_templates/test_with_tables_and_images.docx")
-		assert.Nil(err)
-
-		testImage, err := CreateInlineImage("test_templates/test_image.png")
-		assert.Nil(err)
-
-		profileImage, err := CreateInlineImage("test_templates/test_image.jpg")
-		assert.Nil(err)
-
-		err = profileImage.Resize(100, 100)
-		assert.Nil(err)
-
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-			Image         *InlineImage
-			People        []struct {
-				Name           string
-				Gender         string
-				Age            uint8
-				ProfilePicture *InlineImage
-			}
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-			Image:         testImage,
-			People: []struct {
-				Name           string
-				Gender         string
-				Age            uint8
-				ProfilePicture *InlineImage
-			}{
-				{
-					Name:           "Tom Watkins",
-					Gender:         "Male",
-					Age:            30,
-					ProfilePicture: profileImage,
-				},
-				{
-					Name:           "Evie Argyle",
-					Gender:         "Female",
-					Age:            29,
-					ProfilePicture: profileImage,
-				},
-			},
-		}
-
-		err = doc.Render(data)
-		assert.Nil(err)
-
-		f, err := os.Create("test_templates/generated_test_with_image_structs.docx")
-		assert.Nil(err)
-
-		err = doc.Save(f)
-		assert.Nil(err)
-
-		err = f.Close()
-		assert.Nil(err)
-	})
-
-	t.Run("Document with custom function", func(t *testing.T) {
-		assert := assert.New(t)
-
-		doc, err := ParseFromFilename("test_templates/test_with_custom_function.docx")
-		assert.Nil(err)
-
-		err = doc.RegisterFunction("hello", func(text string) string {
-			return fmt.Sprintf("Hello %v", text)
+			err = f.Close()
+			assert.Nil(err, "Error closing document")
 		})
-		assert.Nil(err)
-
-		data := struct {
-			ProjectNumber string
-			Client        string
-			Status        string
-		}{
-			ProjectNumber: "B-00001",
-			Client:        "TW Software",
-			Status:        "New",
-		}
-
-		err = doc.Render(data)
-		assert.Nil(err)
-
-		f, err := os.Create("test_templates/generated_test_with_custom_function.docx")
-		assert.Nil(err)
-
-		err = doc.Save(f)
-		assert.Nil(err)
-
-		err = f.Close()
-		assert.Nil(err)
-	})
+	}
 }
 
 func parseAndRender(t *testing.T, filename string, data any) {
