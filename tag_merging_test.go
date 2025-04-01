51d85dd867c5f26837331b53e4458b7da62f6676
Tom Watkins <twwatkins@btinternet.com>
2025-04-01 23:03:41 +0100

    Test general tag merging

--- tag_merging_test.go
+++ tag_merging_test.go
@@ -7,6 +7,68 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMergeTagsh(t *testing.T) {
+	assert := assert.New(t)
+
+	pStartText := docx.Text{
+		Text: "{{ .tag ",
+	}
+	pEndText := docx.Text{
+		Text: "}}",
+	}
+	p := docx.Paragraph{
+		Children: []any{
+			&docx.Run{
+				Children: []any{
+					&pStartText,
+					&pEndText,
+				},
+			},
+		},
+	}
+
+	tblStartText := docx.Text{
+		Text: "{{ .tag ",
+	}
+	tblEndText := docx.Text{
+		Text: "}}",
+	}
+	tbl := docx.Table{
+		TableRows: []*docx.WTableRow{
+			{
+				TableCells: []*docx.WTableCell{
+					{
+						Paragraphs: []*docx.Paragraph{
+							{
+								Children: []any{
+									&docx.Run{
+										Children: []any{
+											&tblStartText,
+											&tblEndText,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	items := []any{
+		&p,
+		&tbl,
+	}
+
+	mergeTags(items)
+
+	assert.Equal(pStartText.Text, "")
+	assert.Equal(pEndText.Text, "{{ .tag }}")
+	assert.Equal(tblStartText.Text, "")
+	assert.Equal(tblEndText.Text, "{{ .tag }}")
+}
+
 func TestMergeTagsInParagraph(t *testing.T) {
 	t.Run("Tags in text nodes in same run should get merged", func(t *testing.T) {
 		assert := assert.New(t)
@@ -74,23 +136,22 @@ func TestMergeTagsInTable(t *testing.T) {
 	endText := docx.Text{
 		Text: "}}",
 	}
-	p := docx.Paragraph{
-		Children: []any{
-			&docx.Run{
-				Children: []any{
-					&startText,
-					&endText,
-				},
-			},
-		},
-	}
 	tbl := docx.Table{
 		TableRows: []*docx.WTableRow{
 			{
 				TableCells: []*docx.WTableCell{
 					{
 						Paragraphs: []*docx.Paragraph{
-							&p,
+							{
+								Children: []any{
+									&docx.Run{
+										Children: []any{
+											&startText,
+											&endText,
+										},
+									},
+								},
+							},
 						},
 					},
 				},
