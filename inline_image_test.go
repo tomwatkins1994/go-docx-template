f0127326d1c7888f739802623774094c6d7f6110
tomwatkins1994 <twwatkins@btinternet.com>
2025-04-03 00:12:02 +0100

    Check valid image path

---
+++ inline_image_test.go
@@ -0,0 +1,25 @@
+package docxtpl
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateInlineImage(t *testing.T) {
+	t.Run("Should return error if not a valid image filename", func(t *testing.T) {
+		assert := assert.New(t)
+
+		image, err := CreateInlineImage("test_image.txt")
+		assert.Nil(image)
+		assert.IsType(&InlineImageError{Message: "File is not a valid image"}, err)
+	})
+
+	t.Run("Should return error if not a image doesn't exist", func(t *testing.T) {
+		assert := assert.New(t)
+
+		image, err := CreateInlineImage("image_not_exists.png")
+		assert.Nil(image)
+		assert.IsType(&InlineImageError{Message: "File is not a valid image"}, err)
+	})
+}
