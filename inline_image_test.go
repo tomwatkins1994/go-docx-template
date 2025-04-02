e117c947250e84de323b60dde467bdc8ad94b52e
tomwatkins1994 <twwatkins@btinternet.com>
2025-04-03 00:25:31 +0100

    Test image formats

--- inline_image_test.go
+++ inline_image_test.go
@@ -3,6 +3,7 @@ package docxtpl
 import (
 	"testing"
 
+	"github.com/bep/imagemeta"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -23,5 +24,43 @@ func TestCreateInlineImage(t *testing.T) {
 		assert.Nil(image)
 		assert.IsType(&InlineImageError{Message: "File is not a valid image"}, err)
 		assert.Equal(err.Error(), "Image error: File is not a valid image")
+	})
+}
+
+func TestGetImageFormat(t *testing.T) {
+	t.Run("JPG should return format", func(t *testing.T) {
+		assert := assert.New(t)
+
+		img := &InlineImage{ext: ".jpg"}
+		format, err := img.getImageFormat()
+		assert.Equal(format, imagemeta.JPEG)
+		assert.Nil(err)
+	})
+
+	t.Run("JPEG should return format", func(t *testing.T) {
+		assert := assert.New(t)
+
+		img := &InlineImage{ext: ".jpeg"}
+		format, err := img.getImageFormat()
+		assert.Equal(format, imagemeta.JPEG)
+		assert.Nil(err)
+	})
+
+	t.Run("PNG should return format", func(t *testing.T) {
+		assert := assert.New(t)
+
+		img := &InlineImage{ext: ".png"}
+		format, err := img.getImageFormat()
+		assert.Equal(format, imagemeta.PNG)
+		assert.Nil(err)
+	})
+
+	t.Run("Not image extension should return error", func(t *testing.T) {
+		assert := assert.New(t)
+
+		img := &InlineImage{ext: ".txt"}
+		format, err := img.getImageFormat()
+		assert.Equal(format, imagemeta.ImageFormat(0))
+		assert.NotNil(err)
 	})
 }
